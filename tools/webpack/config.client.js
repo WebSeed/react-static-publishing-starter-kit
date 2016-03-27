932c315513df7ce7f234d253be1d989ba0b052b6
Will Bamford <w.bamford@gmail.com>
2016-03-27 15:07:33 +0100

    Spring clean

--- tools/webpack/config.client.js
+++ tools/webpack/config.client.js
@@ -2,13 +2,9 @@ import path from 'path';
 import loaders from './loaders';
 
 const config = {
-
   target: 'web',
-
   context: path.resolve(__dirname, '../../src'),
-
   entry: './client.js',
-
   output: {
     path: path.resolve(__dirname, '../../build/public/assets'),
     filename: 'client.js'
--- tools/webpack/config.server.js
+++ tools/webpack/config.server.js
@@ -5,13 +5,9 @@ import loaders from './loaders';
 import nodeExternals from 'webpack-node-externals';
 
 const config = {
-
   target: 'node',
-
   context: path.resolve(__dirname, '../../src'),
-
   entry: './server.js',
-
   output: {
     path: path.resolve(__dirname, '../../build'),
     filename: 'server.js',
